f46d10e81e8044a1a373721a1bfdc0af9915d4ee
2025raj <oliraj2025@gmail.com>
2022-05-31 00:16:06 +0545

    just created a called turtle

---
+++ check.py
@@ -0,0 +1,63 @@
+
+# creating a empty dictionary
+'''
+dict_var = {}
+check = isinstance(dict_var, dict)
+print(check)
+'''
+
+# checking:
+'''
+dict_var = {1, 2, 3}
+check = isinstance(dict_var, dict)
+print(check)
+'''
+'''
+# keys must be unique but it's ok to have (same keyword) with different values no error but we lose data @garbage collection occurs..
+# dictionary : allows multiple data-types. 
+ 
+dict_var = {"name": "raj", "age": 3, "height": 5.7,
+            1: "babu", 1.5: "hero", "a": "changed result"}   
+check = isinstance(dict_var, dict)
+print(check)
+
+
+print(dict_var)
+
+'''
+
+'''
+direct_var = {"name": "raj", "age": 20}
+key_val = direct_var["name"]
+print(key_val)
+
+'''
+
+'''
+a = {}
+print(type(a))
+
+b = set()
+print(type(b))
+
+c = ()
+print(type(c))
+
+d = []
+print(type(d))
+'''
+
+'''
+a = int(input("enter a num to find it's factorial: "))
+i = 1
+fact = 1
+while i <= a:
+    fact = fact * i
+    i = i + 1
+print(f"the {a}! is : {fact}")
+
+'''
+Ascen_odr = [18, 1, 55, 2, 30]
+# print(Ascen_odr)
+num = Ascen_odr.sort(reverse=False)
+print(num)
---
+++ turt.py
@@ -0,0 +1,175 @@
+import turtle
+
+# class activity
+#################################################
+# # for making square
+'''import turtle
+wn = turtle.Screen()
+pen = turtle.Turtle()
+for i in range(4):
+    pen.forward(50)
+    pen.left(90)
+turtle.mainloop()
+'''
+
+'''
+import turtle
+wn = turtle.Screen()
+pen = turtle.Turtle()
+for i in range(6):
+
+    pen.right(60)
+    pen.forward(50)
+turtle.mainloop()
+'''
+
+'''
+import turtle
+t = turtle.Turtle()
+t.Screen()
+
+t.up()
+t.goto(0,-100)
+t.down()
+t.circle(100)
+turtle.done()
+turtle.mainloop()
+'''
+
+'''
+import turtle
+turtle.set(0)
+
+turtle.forward(80)
+turtle.write("east")
+
+turtle.forward(80)
+turtle.write("north")
+turtle.home()
+turtle.seth(180)
+'''
+
+
+'''
+wn = turtle.Screen()
+t = turtle.Turtle()
+
+turtle.bgcolor("black")
+turtle.pensize(2)
+turtle.speed(0)
+while(True):
+    for i in range(6):
+        for colors in ["red", "blue", "magenta", "green", "yellow", "white"]:
+            turtle.color(colors)
+            turtle.circle(100)
+            turtle.left(10)
+
+turtle.mainloop()
+'''
+
+
+"""w = turtle.Screen()
+t = turtle.Turtle()
+turtle.mainloop()"""
+
+#####################################
+
+
+'''we use turtle module's "Screen" and " 'T'urtle" built-in function for creating a dedicated screen for my attractive pictures and  shapes '''
+
+
+'''
+# for changing the screensize
+# use this command:
+turtle.screensize()
+turtle.screensize(4000, 5000)
+turtle.screensize()
+'''  # this doesnot work
+turtle.Screen()
+
+# do comment this code using '#' before pen to see differnce
+pen = turtle.Turtle()  # this command is used for pen
+
+
+# pen is (a variable or an object) whereas Turtle built-in function gives us a pen and turtle.Turtle is an (class)
+# turtle modules gives a pen to draw objects in that blank screen
+
+
+# next
+# turtle(pen) has 6 shapes given upper one is classic one
+
+# there are (arrow,turtle, circle, square, triangle, classic)
+
+
+# 1.we can use this by using this given code.
+
+pen.shape("turtle")  # for turtle shape as a pen
+
+
+pen.shape("arrow")  # for arrow shape as a pen
+
+pen.shape("circle")  # for circle as pen
+
+pen.shape("square")  # for square as pen
+
+
+# for changing color of a pen
+
+# use this (object.color"input field" built-in function ) where object is pen
+
+
+pen.color("black")
+pen.forward(50)  # put 0 once to see
+
+
+# pen.color("green")
+# pen.forward(100)
+
+# pen.color("black")
+# pen.forward(100)
+
+
+# for changing the screen color
+# use this code:
+
+turtle.bgcolor("green")  # background color built-in function
+
+# turtle.bgcolor("black")
+
+# turtle.bgcolor("pink")
+
+# for changing the pen size
+pen.pensize(5)
+pen.forward(50)
+
+
+# for changing the pen speed
+
+pen.speed(0)  # speed is used for controlling speed of a pen ;  0 is fastest
+pen.color("black")
+pen.forward(50)
+
+pen.speed(10)  # 10 is for fast
+pen.color("red")
+pen.forward(50)
+
+
+pen.speed(6)  # 6 is for normal
+pen.color("purple")
+pen.forward(50)
+
+pen.speed(3)  # for controlling speed of a pen ;  3 is slow
+pen.color("red")
+pen.forward(50)
+
+pen.speed(1)  # for controlling speed of a pen ;  1 is slowest
+pen.color("red")
+pen.forward(50)
+
+pen.clear()
+
+
+turtle.done()
+
+
+turtle.mainloop()  # only use this command at last
